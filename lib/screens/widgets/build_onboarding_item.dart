1928e92f0c24196261c1084dced934f9461fbb8a
Mohammed Elshafey <106732807+Muhammad-Elshafey@users.noreply.github.com>
2023-12-06 22:55:12 +0200

    product_details_screen.dart

--- lib/screens/widgets/build_onboarding_item.dart
+++ lib/screens/widgets/build_onboarding_item.dart
@@ -2,39 +2,31 @@ import 'package:flutter/material.dart';
 
 import '../../models/onboarding.dart';
 
-Widget buildOnBoardingItem(OnBoardingModel model)=>Padding(
-  padding: const EdgeInsets.all(20.0),
-  child:   Column(
-
-    crossAxisAlignment: CrossAxisAlignment.start,
-
-    children: [
-
-      Container(
-
-        height: 250,
-
-        child: Image(image: AssetImage(model.image!),),
-
+Widget buildOnBoardingItem(OnBoardingModel model) => Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 250,
+            child: Image(
+              image: AssetImage(model.image!),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            model.title!,
+            style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 40),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            model.subtitle!,
+            style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+          ),
+        ],
       ),
-
-      SizedBox(
-
-        height: 10,
-
-      ),
-
-      Text(model.title!,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 40),),
-
-      SizedBox(
-
-        height: 10,
-
-      ),
-
-      Text(model.subtitle!,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 20),),
-
-    ],
-
-  ),
-);
+    );
